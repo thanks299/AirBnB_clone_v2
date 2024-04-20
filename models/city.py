4b2a04c927493f5c2136afca7e80340999713c56
thanks299 <139892876+thanks299@users.noreply.github.com>
2024-04-20 22:54:40 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""City Module for HBNB project"""
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+
+
+class City(BaseModel, Base):
+    """City class representation for database"""
+    __tablename__ = "cities"
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
