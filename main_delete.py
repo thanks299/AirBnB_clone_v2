81f4e04ec486f6e2b699a07c187927295282f52d
thanks299 <thanksagbeble@gmail.com>
2024-04-20 14:06:25 -0700

    airbnb clone

--- main_delete.py
+++ main_delete.py
@@ -7,7 +7,7 @@ from models.state import State
 fs = FileStorage()
 
 # All States
-all_states = fs.all()
+all_states = fs.all(State)
 print("All States: {}".format(len(all_states.keys())))
 for state_key in all_states.keys():
     print(all_states[state_key])
