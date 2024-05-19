a969661ef363854e28406f330256ee4cebeb0973
thanks299 <thanksagbeble@gmail.com>
2024-05-19 03:22:04 -0700

    web flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,51 +1,58 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
 
-from flask import Flask, render_template
+
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
+@app.route('/', strict_slashes=False)
+def flask_hello():
+    """Return string response"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Return response"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False, defaults={"text": "is cool"})
-@app.route("/c/<text>/", strict_slashes=False)
-def c_text(text):
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """Replace underscores with spaces in the text"""
     text = text.replace("_", " ")
-    return "C " + text
+    return "C {}".format(text)
 
-@app.route("/python/", strict_slashes=False, defaults={"text": "is cool"})
-@app.route("/python/<text>/", strict_slashes=False)
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def fl_python(text="is cool"):
+    """Replace underscores with spaces,else use default"""
     text = text.replace("_", " ")
-    return "Python " + text
+    return "Python {}".format(text)
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def number_n(n):
+@app.route('/number/<int:n>')
+def number(n):
+    """Return "{} is a number" with the value of n"""
     return "{} is a number".format(n)
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template_n(n):
-    return render_template("number.html", n=n)
+def html(n):
+    """Displays HTML page if n is an int"""
+    return render_template('5-number.html', n=n)
 
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def number_odd_or_even_n(n):
-    if n % 2 == 0:
-        return render_template("number_odd_or_even.html", n=n, result="even")
-    else:
-        return render_template("number_odd_or_even.html", n=n, result="odd")
+def number_odd_or_even(n):
+    """Displays HTML page if n is an int"""
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
