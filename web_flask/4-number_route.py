4c204939a48b17ed195272a61c3a647d5b2a6b3f
thanks299 <thanksagbeble@gmail.com>
2024-05-19 00:06:50 -0700

    web falsk

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,13 +5,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False, defaults={"text": "is cool"})
 @app.route("/c/<text>/", strict_slashes=False)
@@ -19,15 +22,18 @@ def c_text(text):
     text = text.replace("_", " ")
     return "C " + text
 
+
 @app.route("/python/<text>", strict_slashes=False, defaults={"text": "is cool"})
 @app.route("/python/<text>/", strict_slashes=False)
 def python_text(text):
     text = text.replace("_", " ")
     return "Python " + text
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_n(n):
     return "{} is a number".format(n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
