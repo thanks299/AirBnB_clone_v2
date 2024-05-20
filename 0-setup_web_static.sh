3421c8ba1b1f3a9614543892a9c1a2edb3fb93a0
thanks299 <thanksagbeble@gmail.com>
2024-05-19 18:51:26 -0700

    air clone

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
-# Install Nginx if not already installed
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Create folders and set ownership
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-sudo chown -R ubuntu:ubuntu /data
-
-# Create symbolic link
-sudo ln -sfn /data/web_static/releases/test /data/web_static/current
-
-# Create fake HTML file
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
   <head>
   </head>
@@ -20,8 +18,10 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Update Nginx configuration
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Restart Nginx
 sudo service nginx restart
