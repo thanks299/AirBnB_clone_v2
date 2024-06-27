63417087176078a39a2f0bc5731a95717a1c1ed9
thanks299 <thanksagbeble@gmail.com>
2024-06-27 01:44:41 +0100

    web flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,44 +1,44 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
+"""
+Starts a Flask web application.
+"""
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False, defaults={"text": "is cool"})
-@app.route("/c/<text>/", strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    text = text.replace("_", " ")
-    return "C " + text
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route("/python/", strict_slashes=False, defaults={"text": "is cool"})
-@app.route("/python/<text>/", strict_slashes=False)
-def fl_python(text="is cool"):
-    text = text.replace("_", " ")
-    return "Python " + text
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def number_n(n):
-    return "{} is a number".format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '{} is a number'.format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template_n(n):
-    return render_template("number.html", n=n)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('number_template.html', n=n)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
